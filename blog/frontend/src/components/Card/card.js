52b5947e79e1c42c239ca6584aaab5db9220224a
Juan <juanandres15@gmail.com>
2022-11-15 23:47:33 +0000

    card component

--- blog/frontend/src/components/Card/card.js
+++ blog/frontend/src/components/Card/card.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const card = () => {
   return (
-    <div className='card' id='card'>
+    <card className='card' id='card'>
         <span id='card-author'>Author</span>
         <Date id='card-date'></Date>
         <img id='card--img'></img>
         <h1 id='card--title'>Title Card</h1>
         <p id='card-text'>Card text</p>
-        </div>
+        </card>
   )
 }
 
