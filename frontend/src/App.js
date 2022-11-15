9292ccecb240c985c875a94d925a99d783e7643f
carlozrodri <patica100@gmail.com>
2022-11-15 22:54:59 +0000

    started a react project, added bootrstrap to pakg

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import FistComponent from './components/firstcomponent';
+export default function App() {
+  return (
+    <>
+    <FistComponent/>
+    
+    </>
+  );
+}
+
+
---
+++ frontend/src/components/firstcomponent.jsx
@@ -0,0 +1,11 @@
+import Card from "react-bootstrap/Button";
+
+function FistComponent() {
+  return (
+    <>
+      <Card> primer component</Card>
+    </>
+  );
+}
+
+export default FistComponent;
