9fd3f34b8a1b349467bb4a970d64542ac6bb66ff
Julio Velezmoro <julio28ve@gmail.com>
2022-11-16 01:54:29 +0000

    app

--- blog/frontend/src/components/cards/cards.jsx
+++ blog/frontend/src/components/cards/cards.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 import Card from "../Card/Card";
-var User = "Ferreira91";
-var info = {
-  User: "Ferreira91",
-  Title: "Title of the Blog Postt",
-  Image: "http://",
-  Date: "21/01/2010",
-};
-export default function Cards(props) {
+
+let blogPosts = [
+  {
+    author: "John Doe",
+    date: "12/12/2020",
+    title: "Blog Post 1",
+    src: "https://picsum.photos/200/300",
+    text: "Helloooooo World"
+  },
+  {
+    author: "John Doe",
+    date: "12/12/2020",
+    title: "Blog Post 1",
+    src: "https://picsum.photos/200/300",
+    text: "Helloooooo World"
+  },
+  {
+    author: "John Doe",
+    date: "12/12/2020",
+    title: "Blog Post 1",
+    src: "https://picsum.photos/200/300",
+    text: "Helloooooo World"
+  },
+]
+
+export default function Cards() {
   return (
-
-    <div className="container row p-5 ">
-      <div className="post1 card pl-30 p-2 d-flex ">
-        <div className="d-flex">
-        <p>{info.User}</p>
-
-
-        <p>{info.Date}</p>
-        </div>
-        <img className="card-img-top" src={info.Image} />
-        <div className="text">{info.Title}</div>
-        </div>
-
-      {/* <Card/> */}
+    <div className="cards">
+      {blogPosts.map((post,index) => {
+        return (
+          <Card
+            key={index}
+            author={post.author}
+            date={post.date}
+            title={post.title}
+            src={post.src}
+            text={post.text}
+          />
+        );
+      })}
     </div>
   );
+
 }
