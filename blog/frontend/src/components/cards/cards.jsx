71f7612ffbc76a0d623e909beb5be8a8cd2f8fe6
Jean Ferreira <jeanferreira91@hotmail.com>
2022-11-16 15:11:01 +0000

    style on card

--- blog/frontend/src/components/cards/cards.jsx
+++ blog/frontend/src/components/cards/cards.jsx
@@ -9,20 +9,28 @@ var info = {
 };
 export default function Cards(props) {
   return (
+    <div className="container row p-5 " style={{ maxWidth: "75%" }}>
+      <a
+        href="localhost:3000"
+        style={{ textDecoration: "none", color: "black" }}
+      >
+        <div className="post1 card pl-30 p-2 d-flex ">
+          <div className="d-flex justify-content-between">
+            <p>{info.User}</p>
 
-    <div className="container row p-5 ">
-      <div className="post1 card pl-30 p-2 d-flex ">
-        <div className="d-flex justify-content-between">
-        <p>{info.User}</p>
-
-
-        <p>{info.Date}</p>
+            <p>{info.Date}</p>
+          </div>
+          <img
+            className="card-img-top"
+            style={{ maxHeight: "375px", maxWidth: "768px" }}
+            src={info.Image}
+            alt=""
+          />
+          <div className="text">{info.Title}</div>
         </div>
-        <img className="card-img-top" src={info.Image} />
-        <div className="text">{info.Title}</div>
-        </div>
 
-      {/* <Card/> */}
+        {/* <Card/> */}
+      </a>
     </div>
   );
 }
