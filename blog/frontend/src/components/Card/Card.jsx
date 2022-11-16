63f51e41e4774ef76552e21013cac4b6b9a426b1
Juan <juanandres15@gmail.com>
2022-11-16 00:16:46 +0000

    card structure

--- blog/frontend/src/components/Card/Card.jsx
+++ blog/frontend/src/components/Card/Card.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Card = () => {
   return (
     <div className='card' id='card'>
-        <span id='card-author'>Author</span>
-        <span id='card-date'></span>
-        <img id='card--img'alt='card'/>
-        <h1 id='card--title'>Title Card</h1>
-        <p id='card-text'>Card text</p>
+        <span id='card__author'>Author</span>
+        <span id='card__date'></span>
+        <img id='card__img'alt='card'/>
+        <h1 id='card__title'>Title Card</h1>
+        <p id='card__text'>Card text</p>
         </div>
   )
 }
