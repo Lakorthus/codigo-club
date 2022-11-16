0d02c020ce1f985d5ada7e5a942c2b4f7c89e557
carlozrodri <patica100@gmail.com>
2022-11-16 01:31:40 +0000

    some test

--- blog/frontend/src/components/cards/cards.jsx
+++ blog/frontend/src/components/cards/cards.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import Card from './Card/Card'
-
-const Cards = () => {
+import React from "react";
+import Card from "../Card/Card";
+var User = "Ferreira91";
+var info = {
+  User: "Ferreira91",
+  Title: "Title of the Blog Postt",
+  Image: "http://",
+  Date: "21/01/2010",
+};
+export default function Cards(props) {
   return (
-    <div>cards</div>
-  )
-}
+    <div className="container row p-5 ">
+      <div className="post1 card pl-30 p-2 d-flex ">
+        <div className="d-flex">
+        <p>{info.User}</p>
 
-export default Cards
+        <p>{info.Date}</p>
+        </div>
+        <img className="card-img-top" src={info.Image} />
+        <div className="text">{info.Title}</div>
+        </div>
+
+      {/* <Card/> */}
+    </div>
+  );
+}
