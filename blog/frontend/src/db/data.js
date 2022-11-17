4e7bcb282295d180a5133a2ea8bce91e0f103340
Julio Velezmoro <julio28ve@gmail.com>
2022-11-17 23:54:25 +0000

    card resposive and db

---
+++ blog/frontend/src/db/data.js
@@ -0,0 +1,26 @@
+export const data =[
+    {
+      author: "John Doe",
+      date: "12/12/2020",
+      title: "Blog Post 1",
+      src: "https://picsum.photos/200/300",
+      text: "Helloooooo World"
+    },
+    {
+      author: "John Doe",
+      date: "12/12/2020",
+      title: "Blog Post 1",
+      src: "https://picsum.photos/200/300",
+      text: "Helloooooo World",
+    },
+    {
+      author: "John Doe",
+      date: "12/12/2020",
+      title: "Blog Post 1",
+      src: "https://picsum.photos/200/300",
+      text: "Helloooooo World",
+    }
+]
+
+
+
