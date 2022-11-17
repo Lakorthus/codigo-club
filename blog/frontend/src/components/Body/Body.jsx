1f0165d19ff1859419c0db67e9db7671fe44c5c6
Julio Velezmoro <julio28ve@gmail.com>
2022-11-17 22:34:07 +0000

    bostrap cards

--- blog/frontend/src/components/Body/Body.jsx
+++ blog/frontend/src/components/Body/Body.jsx
@@ -4,13 +4,12 @@ import Card from "../Card/Card";
 
 export default function Body(props) {
   return (
-    <div className="row d-flex justify-content-between">
+    <div className="row d-flex flex-column flex-lg-row justify-content-between">
       <div className="col-8">
         <Card />
       </div>
-      <div className="col-4">
         <Cards />
-      </div>
+      
     </div>
   );
 }
--- blog/frontend/src/components/Card/Card.jsx
+++ blog/frontend/src/components/Card/Card.jsx
@@ -1,68 +1,25 @@
 import React from "react";
 
-<<<<<<< HEAD
 const Card = (props) => {
   return (
     <div className="container ps-3 pe-3 pb-1 pt-4">
-      <div className="card d-flex" 
->
-          <div className="d-flex ms-1 me-1 justify-content-between p-2">
+      <div className="card d-flex">
+        <div className="d-flex ms-1 me-1 justify-content-between p-2">
           <p>{props.author}</p>
 
           <p>{props.date}</p>
         </div>
-        <img className="img-fluid" 
-                    style={{
-                      width: '100%',
-                      height: '20rem'
-                    }} 
-                    src={props.src} 
-                    alt={props.author}/>
-                 
+        <img
+          className="img-fluid"
+          src={props.src}
+          alt={props.author}
+        />
+
         <p className="text">{props.title}</p>
         <p className="text">{props.text}</p>
-
       </div>
     </div>
   );
 };
 
-export default Card;  
-=======
-var User = "Ferreira91";
-var info = {
-  User: "Ferreira91",
-  Title: "Title of the Blog Postt",
-  Image: "https://ychef.files.bbci.co.uk/976x549/p0202jwh.jpg",
-  Date: "21/01/2010",
-  Text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-};
-export default function Card(props) {
-  return (
-    <div className="container row p-5 " style={{ width: "100%", maxHeigth: "100vh"}}>
-      <a
-        href="localhost:3000"
-        style={{ textDecoration: "none", color: "black" }}
-      >
-        <div className="post1 card pl-30 p-2 d-flex" style={{ gap: "5px"}}>
-          <div className="text">{info.Title}</div>
-          <img
-            className="card-img-top"
-            style={{ maxHeight: "375px", maxWidth: "100%" }}
-            src={info.Image}
-            alt=""
-          />
-          <div className="d-flex justify-content-between">
-            <p>{info.User}</p>
-
-            <p>{info.Date}</p>
-          </div>
-          <div className="text" style={{ textAlign: "justify"}}>{info.Text}</div>
-        </div>
-
-        {/* <Card/> */}
-      </a>
-    </div>
-  );
-}
->>>>>>> Card-Juan
+export default Card;
