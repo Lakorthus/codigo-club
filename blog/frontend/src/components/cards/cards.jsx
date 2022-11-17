5a8bfbe05edbf8d1c58a1b120c40695355aa54c7
Jean Ferreira <jeanferreira91@hotmail.com>
2022-11-17 14:55:26 +0000

    Update

--- blog/frontend/src/components/cards/cards.jsx
+++ blog/frontend/src/components/cards/cards.jsx
@@ -1,78 +1,46 @@
 import React from "react";
+import Card from "../Card/Card";
 
-var User = "Ferreira91";
-var info = {
-  User: "Ferreira91",
-  Title: "Title of the Blog Postt",
-  Image: "https://ychef.files.bbci.co.uk/976x549/p0202jwh.jpg",
-  Date: "21/01/2010",
-};
-export default function Cards(props) {
+let blogPosts = [
+  {
+    author: "John Doe",
+    date: "12/12/2020",
+    title: "Blog Post 1",
+    src: "https://picsum.photos/200/300",
+    text: "Helloooooo World"
+  },
+  {
+    author: "John Doe",
+    date: "12/12/2020",
+    title: "Blog Post 1",
+    src: "https://picsum.photos/200/300",
+    text: "Helloooooo World"
+  },
+  {
+    author: "John Doe",
+    date: "12/12/2020",
+    title: "Blog Post 1",
+    src: "https://picsum.photos/200/300",
+    text: "Helloooooo World"
+  },
+]
+
+export default function Cards() {
   return (
-    <div className="container row p-5 " style={{ maxWidth: "375px", maxHeigth : "100vh", gap : '20px'}}>
-      <a
-        href="localhost:3000"
-        style={{ textDecoration: "none", color: "black" }}
-      >
-        <div className="post1 card pl-30 p-2 d-flex ">
-          <div className="d-flex justify-content-between">
-            <p>{info.User}</p>
-
-            <p>{info.Date}</p>
-          </div>
-          <img
-            className="card-img-top"
-            style={{ maxHeight: "375px", maxWidth: "768px" }}
-            src={info.Image}
-            alt=""
+    <div className="cards">
+      {blogPosts.map((post,index) => {
+        return (
+          <Card
+            key={index}
+            author={post.author}
+            date={post.date}
+            title={post.title}
+            src={post.src}
+            text={post.text}
           />
-          <div className="text">{info.Title}</div>
-        </div>
-
-        {/* <Card/> */}
-      </a>
-      <a
-        href="localhost:3000"
-        style={{ textDecoration: "none", color: "black" }}
-      >
-        <div className="post1 card pl-30 p-2 d-flex ">
-          <div className="d-flex justify-content-between">
-            <p>{info.User}</p>
-
-            <p>{info.Date}</p>
-          </div>
-          <img
-            className="card-img-top"
-            style={{ maxHeight: "375px", maxWidth: "768px" }}
-            src={info.Image}
-            alt=""
-          />
-          <div className="text">{info.Title}</div>
-        </div>
-
-        {/* <Card/> */}
-      </a>
-      <a
-        href="localhost:3000"
-        style={{ textDecoration: "none", color: "black" }}
-      >
-        <div className="post1 card pl-30 p-2 d-flex ">
-          <div className="d-flex justify-content-between">
-            <p>{info.User}</p>
-
-            <p>{info.Date}</p>
-          </div>
-          <img
-            className="card-img-top"
-            style={{ maxHeight: "375px", maxWidth: "768px" }}
-            src={info.Image}
-            alt=""
-          />
-          <div className="text">{info.Title}</div>
-        </div>
-
-        {/* <Card/> */}
-      </a>
+        );
+      })}
     </div>
   );
+
 }
