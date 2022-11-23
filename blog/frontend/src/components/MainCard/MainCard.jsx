c9df4526c86f83fa40b345d43badcc1aed838cb4
Juan <juanandres15@gmail.com>
2022-11-23 01:24:31 +0000

    update

--- blog/frontend/src/components/MainCard/MainCard.jsx
+++ blog/frontend/src/components/MainCard/MainCard.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import './MainCard.css'
-
+import React from "react";
+import "./MainCard.css";
 
 const MainCard = (props) => {
   return (
-    <div class="card">
-        <img className="card-img-top style-main-img" src={props.src} alt={props.author} />
-        <div className="card-body">
-          <h5 class="card-title">{props.title}</h5>
-          <p>{props.author}</p>
-          <p className="card-text">{props.text}</p>
-          <p class="card-text">
-            <small class="text-muted">{props.date}</small>
-          </p>
-        </div>
+    <div className="card">
+      <img
+        className="card-img-top style-main-img"
+        src={props.src}
+        alt={props.author}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p>{props.author}</p>
+        <p className="card-text">{props.text}</p>
+        <p className="card-text">
+          <small className="text-muted">{props.date}</small>
+        </p>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default MainCard
+export default MainCard;
