56629e0882d97d9a10da848a8909c90281fa99db
juanf1990 <juanandres15@gmail.com>
2022-12-23 22:19:35 +0000

    development tests

--- codigo-club/src/pages/home.jsx
+++ codigo-club/src/pages/home.jsx
@@ -9,7 +9,10 @@ export default function Home() {
       <Body className="flex flex-col md:flex-col-reverse">
         <Main />
         <Header
-          paragraph="Codigo Club is a programming club dedicated to helping aspiring coders learn the fundamentals of coding. Our blog provides insightful and informative content on everything from the basics of programming to the latest trends in the coding world.We also provide tutorials, resources, and discussion boards to help our members stay up to date with the latest coding practices. We are committed to helping our members become the best coders they can be and create amazing applications"
+          title="Codigo Club is a programming club dedicated to helping aspiring coders learn the fundamentals of coding.
+          Our blog provides insightful and informative content on everything from the basics of programming to the latest trends in the coding world.We also provide tutorials, resources, and discussion boards to help our members stay up to date with the latest coding practices.
+          We are committed to helping our members become the best coders they can be and create amazing applications"
+          className="text-[2em]"
         />
       </Body>
     </>
