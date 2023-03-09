a8a8ca2a123f58dd92301f3e9947d843487f95da
Julio Velezmoro <julio28ve@gmail.com>
2023-03-09 22:52:21 +0000

    build: initial commit codigo club blog

---
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -0,0 +1,25 @@
+import Header from '../../navigation/header/Header';
+
+export interface IPrimaryLayout extends React.ComponentPropsWithoutRef<'div'> {
+  children: React.ReactNode;
+  justify?: 'items-center' | 'items-start';
+}
+
+const PrimaryLayout: React.FC<IPrimaryLayout> = ({
+  children,
+  justify = 'items-center',
+  ...divProps
+}) => {
+  return (
+    <>
+      <div {...divProps} className={`min-h-screen flex flex-col ${justify}`}>
+        <Header />
+        <main className="relative w-full flex-1 flex justify-center items-center">
+          {children}
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default PrimaryLayout;
